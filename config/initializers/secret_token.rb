599225401798bd6378285158b3c27feb0a39e286
Tim Lucas <t.lucas@toolmantim.com>
2012-02-11 11:30:47 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetPipelineExample::Application.config.secret_token = '7fe4389d90df53d1c8cb9cfa3dca80fc8bbbd757cb90a59f07e87677fddc60306660f051486b144efc51770fa8f666759a0635294b3f4eebf3eb5b3bd0053576'
